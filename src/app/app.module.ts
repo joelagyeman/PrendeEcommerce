aba0f3733d2e3a2bf5de48a650f1e6daae9b3e8b
Joel Bill Agyeman-Mensah <joelagyeman@meltwater.org>
2023-01-10 15:34:27 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './app-core/common/header/header.component';
+import { FooterComponent } from './app-core/common/footer/footer.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { ForgotPasswordComponent } from './components/auth/forgot-password/forgot-password.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    RegisterComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
